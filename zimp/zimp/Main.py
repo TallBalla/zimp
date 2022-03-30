54ebfaab9961edf3487832f4c62d6045350055ee
Will Russ-Hofmans <wrr0031@arastudent.ac.nz>
2022-03-30 23:28:45 +1300

    Refactored code

--- zimp/zimp/Main.py
+++ zimp/zimp/Main.py
@@ -1,6 +1,4 @@
 from Commands import Commands
-from Database import Database
 
 if __name__ == "__main__":
-    Database().connect_to_database()
     Commands().cmdloop()
--- zimp/zimp/View.py
+++ zimp/zimp/View.py
@@ -19,7 +19,7 @@ class View():
 
     def error(self, text):
         print(colored(f'\n{text}', 'red'))
-    
+
     def display_start_heading(self):
         self.heading('Starting')
         self.sub_heading('Type help or "?" to list the commands or "start" to start the game')
@@ -32,8 +32,33 @@ class View():
               f'The game state is {state}\n')
     
     def display_game(self, chosen_tile, state, avail_doors, suggest_cmd):
-        print(f'Current Room: {chosen_tile.name}\n'
+        print(f'\nCurrent Room: {chosen_tile.name}\n'
               f'Available Doors: {avail_doors}\n'
-              f'Current Player State: {state}\n'
               f'Special Entrances : {chosen_tile.entrance}\n'
+              f'Current Player State: {state}\n'
               f'Suggested Command: {suggest_cmd}\n')
+
+    def warning_dinning_room_exit(self):
+        self.warning("Dining room entrance must face an empty tile")
+
+    def warning_enter_valid_file_name(self):
+        self.warning('Must enter a valid file name')
+
+    def warning_invalid_run_direction(self):
+        self.warning('Must enter a vaild direction')
+
+    def error_player_cannot_move(self):
+        self.error('Player not ready to move')
+
+    def error_cannot_cower(self):
+        self.error('Cannot cower while right now')
+
+    def error_cannot_runaway(self):
+        self.error('Cannot runaway currently')
+
+    def error_cannot_search(self):
+        self.error('Cannot search for items currently')
+
+    def error_file_does_not_exist(self, file_name):
+        self.error(f'{file_name} file does not exist, '
+                   'try another file or create file')
